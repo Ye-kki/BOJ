f306c6488f5e94fb3d663fbffaa149008b80fe70
Ye-kki <yechan_choi@naver.com>
2023-01-24 23:01:00 +0900

    2023-01-24

---
+++ 자료구조/13915_현수의열기구교실.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+#include <string>
+#include <set>
+
+using namespace std;
+
+int N;
+set<int> s;
+set<string> ans;
+
+int main() {
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	while (cin >> N) {
+		ans.clear();
+		for (int i = 0; i < N; i++) {
+			string s1, s2;
+			s.clear();
+			cin >> s1;
+			for (int j = 0; j < s1.size(); j++) s.insert(s1[j]);
+			for (auto i : s) s2 += i;
+			ans.insert(s2);
+		}
+		cout << ans.size() << '\n';
+	}
+}
